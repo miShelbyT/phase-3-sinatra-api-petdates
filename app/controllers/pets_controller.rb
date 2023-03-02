80e27ba8b96e7a506d426961d60c207a968e1a38
Shelby Talbert <shelby.talbert@flatironschool.com>
2023-03-01 22:50:22 -0500

    updated pets show controller method to eager load meetup pet names

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,12 +1,18 @@
 class PetsController < ApplicationController
 
   get "/pets" do
-    Pet.all.to_json(only: [:id, :name, :species, :breed, :image_url], methods: [:age])
+    Pet.all.to_json(only: [:id, :image_url])
   end
 
   get "/pets/:id" do
     pet = Pet.find(params[:id])
-    pet.to_json(include: {:meetups => {methods: [:formatted_time]}}, methods: [:age])
+    pet.to_json(methods: [:age], include: {:meetups => 
+      { methods: [:formatted_time], include: {:pets => { only: :name }}}
+      })
+  end
+
+  post "/pets" do
+    binding.pry
   end
 
 end
